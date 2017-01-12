c528a773b198a77972312cb9c2ec1318a64949b3
Saurabh <saurabharora90@gmail.com>
2017-01-12 23:43:40 +0800

    display flight timing

--- Scoot/spiders/MainPage_spider.py
+++ Scoot/spiders/MainPage_spider.py
@@ -22,10 +22,10 @@ class MainPage(scrapy.Spider):
         'revAvailabilitySearch.SearchInfo.SalesCode' : '',
         'revAvailabilitySearch.SearchInfo.SearchStations[0].DepartureStationCode' : self.departure,
         'revAvailabilitySearch.SearchInfo.SearchStations[0].ArrivalStationCode' : self.destination,
-        'revAvailabilitySearch.SearchInfo.SearchStations[0].DepartureDate':	'01/27/2017',
+        'revAvailabilitySearch.SearchInfo.SearchStations[0].DepartureDate':	'04/13/2017',
         'revAvailabilitySearch.SearchInfo.SearchStations[1].DepartureStationCode' : self.destination,
         'revAvailabilitySearch.SearchInfo.SearchStations[1].ArrivalStationCode' : self.departure,
-        'revAvailabilitySearch.SearchInfo.SearchStations[1].DepartureDate' : '01/31/2017'}
+        'revAvailabilitySearch.SearchInfo.SearchStations[1].DepartureDate' : '04/16/2017'}
         yield FormRequest.from_response(response,
                                 formxpath='//*[@id="searhflightform_return"]',
                                 formdata=formdata,
@@ -34,15 +34,18 @@ class MainPage(scrapy.Spider):
 
     def bookingPage(Self, response):
         pathForDepartFlights = '/html/body/div[1]/main/form[1]/section[1]/div[3]/div[1]/div'
+        timingForDepartFlight = '/html/body/div[1]/main/form[1]/section[1]/div[3]/div[3]/div[1]/div[1]/ul/li/text()'
+
         pathForReturnFlights = '/html/body/div[1]/main/form[1]/section[2]/div[3]/div[1]/div'
+        timingForReturnFlight = '/html/body/div[1]/main/form[1]/section[2]/div[3]/div[3]/div[1]/div[1]/ul/li/text()'
 
         currentFlightDepart = response.xpath(pathForDepartFlights).xpath('div[@class="tab active"]')
         departDay = currentFlightDepart.xpath('a/span/text()').extract()[0].strip()
         departCost = currentFlightDepart.xpath('a/span/strong/text()').extract()[0].strip()
-        print "Flight {2} on {0} is {1} ".format(departDay, departCost, response.xpath('/html/body/div[1]/main/form[1]/section[1]/div[1]/div/div[1]/h2/text()').extract()[0].strip())
+        print "Flight {2} on {0} at {3} is {1} ".format(departDay, departCost, response.xpath('/html/body/div[1]/main/form[1]/section[1]/div[1]/div/div[1]/h2/text()').extract()[0].strip(), response.xpath(timingForDepartFlight).extract()[0].strip())
 
         currentFlightReturn = response.xpath(pathForReturnFlights).xpath('div[@class="tab active"]')
         returnDay = currentFlightReturn.xpath('a/span/text()').extract()[0].strip()
         returnCost = currentFlightReturn.xpath('a/span/strong/text()').extract()[0].strip()
-        print "Flight {2} on {0} is {1} ".format(returnDay, returnCost, response.xpath('/html/body/div[1]/main/form[1]/section[2]/div[1]/div/div[1]/h2/text()').extract()[0].strip())
+        print "Flight {2} on {0} at {3} is {1} ".format(returnDay, returnCost, response.xpath('/html/body/div[1]/main/form[1]/section[2]/div[1]/div/div[1]/h2/text()').extract()[0].strip(), response.xpath(timingForReturnFlight).extract()[0].strip())
         #scrapy.utils.response.open_in_browser(response)
